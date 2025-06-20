680a1e99018b3486e293c81e9b221e2a9a4bb804
lizepeng1 <lizepeng1@genomics.cn>
2025-06-20 14:50:06 +0800

    # manual registration & auto registration -- update.

--- cellbin2/contrib/calibration.py
+++ cellbin2/contrib/calibration.py
@@ -5,6 +5,7 @@
 🌟 File  : calibrate.py
 🌟 Description  :
 """
+import os
 import math
 import imreg_dft
 
@@ -36,6 +37,8 @@ class Calibrate:
             self,
             src_image: Union[str, np.ndarray] = None,
             dst_image: Union[str, np.ndarray] = None,
+            same_image: Union[str, np.ndarray] = None,
+            output_path: str = '',
             method: int = 0,
             down_size: int = 4000
     ):
@@ -55,6 +58,9 @@ class Calibrate:
 
         self.src_image = self.parse_img(src_image)
         self.dst_image = self.parse_img(dst_image)
+
+        self.same_image = self.parse_img(same_image)
+        self.output_path = output_path
 
         self.down_size = down_size
 
@@ -169,6 +175,37 @@ class Calibrate:
     def set_dst(self, im):
         self.dst_image = self.parse_img(im)
 
+    @staticmethod
+    def get_mass(image):
+        """
+
+        Args:
+            image:
+
+        Returns:
+
+        """
+        M = cv.moments(image)
+        cx_cv = round(M['m10'] / M['m00'])
+        cy_cv = round(M['m01'] / M['m00'])
+
+        return np.array([cx_cv, cy_cv])
+
+    def mass_align(self):
+        """
+
+        Returns:
+
+        """
+        src_mass, dst_mass = self.get_mass(self.src_image), self.get_mass(self.dst_image)
+        offset = src_mass - dst_mass
+
+        _di = cbimread(self.dst_image)
+
+        _di = _di.trans_image(offset=offset, dst_size = self.src_image.shape)
+
+        return _di.image
+
     def calibration(self):
         """
         *Scale and size the image uniformly
@@ -177,11 +214,32 @@ class Calibrate:
         Returns:
 
         """
-        down_scale = max(self.src_image.shape) / self.down_size
 
-        self.src_image, self.dst_image = self._consistent_image(
-            self.src_image, self.dst_image, 'same'
+        norm_scale = min(self.src_image.shape) / max(self.dst_image.shape)
+        self.dst_image = cv.resize(
+            self.dst_image,
+            (int(self.dst_image.shape[1] * norm_scale), int(self.dst_image.shape[0] * norm_scale))
         )
+
+        # 一致性
+        self.dst_image = self.mass_align()
+        # self.src_image, self.dst_image = self._consistent_image(
+        #     self.src_image, self.dst_image, 'same'
+        # )
+
+        # 填充
+        # pad_rate = 0.1
+        # padding_size = int(max(self.src_image.shape) * pad_rate)
+        # self.src_image = cv.copyMakeBorder(
+        #     self.src_image, padding_size, padding_size, padding_size, padding_size,
+        #     borderType = cv.BORDER_CONSTANT, value=0
+        # )
+        # self.dst_image = cv.copyMakeBorder(
+        #     self.dst_image, padding_size, padding_size, padding_size, padding_size,
+        #     borderType = cv.BORDER_CONSTANT, value=0
+        # )
+
+        down_scale = max(self.src_image.shape) / self.down_size
 
         src_img = self.resize_image(
             self.src_image, 1 / down_scale
@@ -201,32 +259,42 @@ class Calibrate:
         scale = ret.get('scale', 1)
         rotate = ret.get('angle', 0)
 
-        # trans_mat[:2, 2] = offset
         trans_info = {"score": score, "offset": offset, "scale": scale, "rotate": rotate}
-        # new_dst = imreg_dft.transform_img(img=self.dst_image, scale=scale, angle=rotate, tvec=offset[::-1])
-        new_dst = cbimread(self.dst_image)
+
+        if self.same_image is not None:
+            new_dst = cbimread(self.same_image)
+        else:
+            new_dst = cbimread(self.dst_image)
         new_dst = new_dst.trans_image(
             scale = float(scale),
             rotate = rotate,
         )
-        new_dst = new_dst.trans_image(
-            offset = offset,
-            dst_size = new_dst.shape
+
+        _offset = (np.array(self.src_image.shape[:2]) - np.array(new_dst.shape[:2])) / 2
+        _offset = _offset[::-1]
+
+        result = new_dst.trans_image(
+            offset = offset + _offset,
+            dst_size = self.src_image.shape[:2]
         )
 
-        _, result = self._consistent_image(self.src_image, new_dst.image, 'same')
+        # _, result = self._consistent_image(self.src_image, new_dst.image, 'same')
+        if len(self.output_path) > 0:
+            result.write(self.output_path)
 
-        return result, trans_info
+        return result.image, trans_info
 
 
 def multi_channel_align(
         cfg: CalibrationParam,
         fixed_image: str,
         moving_image: str,
+        same_image: str = '',
+        output_path: str = '',
         method: int = 0
 ) -> CalibrationInfo:
     assert method in [0, 1]
-    cal = Calibrate(fixed_image, moving_image, method=method)
+    cal = Calibrate(fixed_image, moving_image, same_image, output_path, method=method)
     new_dst, trans_info = cal.calibration()
     x, y = trans_info['offset']
     d = math.sqrt(x * x + y * y)
@@ -237,7 +305,7 @@ def multi_channel_align(
 
 def main(args):
     cfg = CalibrationParam()
-    multi_channel_align(cfg, args.src_image, args.dst_image, method=args.method)
+    multi_channel_align(cfg, args.src_image, args.dst_image, args.same_image, args.output, method=args.method)
 
 
 if __name__ == "__main__":
@@ -249,8 +317,15 @@ if __name__ == "__main__":
                         help="Src image path.")
     parser.add_argument("-dst", "--dst_image", action="store", dest="dst_image", type=str, required=True,
                         help="Dst image path.")
+
+    parser.add_argument("-same", "--same_image", action = "store", dest = "same_image", type = str, required = False,
+                        default = '', help = "Same trans image path.")
+
     parser.add_argument("-m", "--method", action="store", dest="method", type=int, required=False, default=0,
                         help="Translation = 0 | Similarity = 1.")
+
+    parser.add_argument("-o", "--output", action="store", dest="output", type=str, required=False, default = "",
+                        help="Result output dir.")
 
     parser.set_defaults(func=main)
     (para, _) = parser.parse_known_args()
